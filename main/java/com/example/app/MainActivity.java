c603bbd1239443537b13f8f1864041d1bdc03a93
gourishankar <48941302+gourishankar55@users.noreply.github.com>
2020-10-13 19:00:38 +0530

    Add files via upload
    This is task 1

---
+++ main/java/com/example/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "connection successfull", Toast.LENGTH_SHORT).show(); 
+
+    }
+}
